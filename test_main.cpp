45a865f01f3a4eff4f45c90b69b6ec515b2a8dac
rfrolov <rfrolov@engineer.com>
2018-06-26 16:06:39 +0300

    Тесты теперь проводятся с помощью GTest.

--- test_main.cpp
+++ test_main.cpp
@@ -1,14 +1,13 @@
 #include "lib.h"
+#include "gtest/gtest.h"
 
-#define BOOST_TEST_MODULE test_main
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_suite_main)
-
-BOOST_AUTO_TEST_CASE(test_version_valid)
+TEST(test_case, check_version)
 {
-	BOOST_CHECK( version() > 0 );
+    EXPECT_GT(version(), 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
